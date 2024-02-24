5c6648ac1a09f1f3dd958afde7ab9fddd993830b
sergelorenz <sergelorenz.villasica@gmail.com>
2024-02-24 22:55:04 +0800

    - Implemented email_data_reader service which is designed to parse email data from multiple sources and extract the subject and its body and saves as a csv file

--- emailclassifier/emailuploader/admin.py
+++ emailclassifier/emailuploader/admin.py
@@ -1,12 +1,19 @@
+from io import BytesIO
 from django.contrib import admin
-from .models import Email, EmailFile
+from .models import EmailFile
+from .services.email_data_reader import EmailCSVReader
+
 
 # Register your models here.
-admin.site.register(Email)
-
-
 class EmailFileAdmin(admin.ModelAdmin):
-    list_display = ('email_file', )
+    list_display = ('email_file', "file_type")
+
+    def save_model(self, request, obj, form, change):
+        file = obj.email_file.file.file
+        io_reader = EmailCSVReader()
+        processed_email_obj = io_reader.read_email(file)
+        obj.email_file.file = processed_email_obj
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(EmailFile, EmailFileAdmin)
