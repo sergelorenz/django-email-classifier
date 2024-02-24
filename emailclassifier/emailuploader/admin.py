144533d1c75b5e3695347a6574df517fee03078d
sergelorenz <sergelorenz.villasica@gmail.com>
2024-02-24 16:02:45 +0800

    - Added email file upload in django admin - Added emailfile model and registered it in admin

--- emailclassifier/emailuploader/admin.py
+++ emailclassifier/emailuploader/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Email
+from .models import Email, EmailFile
 
 # Register your models here.
 admin.site.register(Email)
+
+
+class EmailFileAdmin(admin.ModelAdmin):
+    list_display = ('email_file', )
+
+
+admin.site.register(EmailFile, EmailFileAdmin)
