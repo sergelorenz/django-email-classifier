c0f351664cdef6974e0eb7d3b3b3e45ca7fd5687
sergelorenz <sergelorenz.villasica@gmail.com>
2024-02-25 22:46:25 +0800

    - perform_classification logic using initial Mock Classifier - Populate ClassificationResults after classification

--- emailclassifier/classifier/admin.py
+++ emailclassifier/classifier/admin.py
@@ -1,9 +1,13 @@
 from django.core.exceptions import ValidationError
-from django.contrib import admin
+from django.contrib import admin, messages
 from django import forms
 
-from .models import Classification
+from .models import Classification, ClassificationResults
 from .services.file_reader import LocalFileReader
+from .services.mock_classifier_service import MockClassifier
+
+
+admin.site.disable_action('delete_selected')
 
 
 class ClassificationForm(forms.ModelForm):
@@ -33,13 +37,34 @@ class ClassificationAdmin(admin.ModelAdmin):
         obj.save()
         return fields
 
+    # ########### PLEASE READ ####################
+    # Due to time constraints, this is only done synchronously which would cause UI blocking Especially on using ML
+    # Using Celery with Redis/RabbitMQ would drastically improve the user experience for this action
     @admin.action(description='Perform classification on the selected items')
     def perform_classification(self, request, queryset):
+        if queryset.count() > 1:
+            messages.error(request, 'You can only select one item at a time')
+            return
+
+        ClassificationResults.objects.all().delete()
         for obj in queryset:
+            new_results = []
+            classifier = MockClassifier()
             file_path = obj.email_file.email_file.path
             file_reader = LocalFileReader()
             for row in file_reader.read_line(file_path):
-                pass
+                if len(row) == 2:
+                    subject, body = row
+                    email_class = classifier.classify(subject + body)
+                    result = ClassificationResults(
+                        email_subject=subject,
+                        email_body=body,
+                        email_class=email_class.name
+                    )
+                    new_results.append(result)
+
+            ClassificationResults.objects.bulk_create(new_results)
+            messages.success(request, 'Successfully performed classification')
 
 
 admin.site.register(Classification, ClassificationAdmin)
